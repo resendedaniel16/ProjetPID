80af20f8e66e810787df0158cecdf41d75219c5e
daniel resende <resende_daniel@live.be>
2025-10-15 18:41:57 +0200

    After framework installation

---
+++ src/main/java/com/projetReservations/ProjetReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.projetReservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetReservations/ProjetReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetReservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
